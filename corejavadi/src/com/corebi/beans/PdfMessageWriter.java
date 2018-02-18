a962dc8e73cf6bebbad4d9847b193abd07df03ab
shekhfirozalam <firozhere@gmail.com>
2018-02-18 16:49:16 +0530

    corejavaDi

---
+++ corejavadi/src/com/corebi/beans/PdfMessageWriter.java
@@ -0,0 +1,9 @@
+package com.corebi.beans;
+
+import com.corebi.interfacemessge.MessageWriter;
+
+public class PdfMessageWriter implements MessageWriter {
+
+
+
+}
---
+++ corejavadi/src/com/corebi/interfacemessge/MessageWriter.java
@@ -0,0 +1,6 @@
+package com.corebi.interfacemessge;
+
+public interface MessageWriter {
+	public String PdfmessageWriter();
+	public String HtmlmessageWriter();
+}
