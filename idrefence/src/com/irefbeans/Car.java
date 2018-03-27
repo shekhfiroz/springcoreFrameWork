67037ae0e97250481db22cb68621f6d6112e963f
shekhfirozalam <firozhere@gmail.com>
2018-03-27 16:41:32 +0530

    idref edit

--- idrefence/src/com/irefbeans/Car.java
+++ idrefence/src/com/irefbeans/Car.java
@@ -7,8 +7,7 @@ import org.springframework.core.io.ClassPathResource;
 public class Car {
 	private IEngine engine;
 	private String engineId;
-	
-   
+
 	public void setEngineId(String engineId) {
 		this.engineId = engineId;
 	}
@@ -18,10 +17,11 @@ public class Car {
 	}
 
 	public void drive() {
-		System.out.println("drive() method is called.." );
-		//we are creating container object to varify initially to save our application to terminate at run time
-		BeanFactory factory=new XmlBeanFactory(new ClassPathResource("com/iref/common/application-context.xml"));
-		engine=factory.getBean(engineId,IEngine.class);
+		System.out.println("drive() method is called..");
+		// we are creating container object to varify initially to save our application
+		// to terminate at run time
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("com/iref/common/application-context.xml"));
+		engine = factory.getBean(engineId, IEngine.class);
 		engine.accelerate("100");
 	}
 
